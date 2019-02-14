0baf79c14565835ddb2e2851308088bf620f7528
Tony Cheung <iamtonycheung@gmail.com>
2019-02-15 00:00:22 +0800

    first commit

---
+++ ProTwo/appTwo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+# from appTwo.models import User
+
+from appTwo.forms import NewUserForm
+# Create your views here.
+
+def index(request):
+    return render(request,'apptwo/index.html')
+
+def users(request):
+
+    form = NewUserForm()
+
+    if request.method =="POST":
+        form=NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Erro Form Invalid')
+
+    return render(request,'appTwo/users.html',{'form':form})    
+
+
+    # user_list = User.objects.order_by('first_name')
+    # user_dict = {"users":user_list}
+    # return render(request,'apptwo/users.html',context=user_dict)
